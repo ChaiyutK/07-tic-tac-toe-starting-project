af67842eb73d70662842d91c06d51de1fa975fdd
Chaiyut <benzchaiyut.k@gmail.com>
2024-10-03 00:05:01 +0700

    change li of ol class player to component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
+import Player from'./components/Player.jsx'
 
 function App() {
-  
-
   return (
     <main>
       <div id="game-container">
         <ol id="players">
-            <li>
-              <span className="player-name">Player 1</span>
-              <span className="player-symbol">X</span>
-            </li>
-            <li>
-              <span className="player-name">Player 2</span>
-              <span className="player-symbol">O</span>
-            </li>
+          <Player name="Benz" symbol="X"/>
+          <Player name="Ant" symbol="O"/>
         </ol>
-
         GAME BOARD
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
