6b3562cb1df34666c59160639a01fff9f333d9b6
Chaiyut <benzchaiyut.k@gmail.com>
2024-10-06 00:25:48 +0700

    add hightlight at activeplayer side

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import Player from'./components/Player.jsx'
 import GameBoard from './components/Gameboard.jsx';
+import { useState } from 'react';
 
 function App() {
+  const [activePlayer,setActivePlayer] = useState('X');
+
+  function handleSelectSquare(){
+    setActivePlayer((currentActivePlayer)=> currentActivePlayer === 'X'?'O':'X')
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Benz" symbol="X"/>
-          <Player initialName="Ant" symbol="O"/>
+        <ol id="players" className="highlight-player">
+          <Player initialName="Benz" symbol="X" isActive={activePlayer==='X'}/>
+          <Player initialName="Ant" symbol="O" isActive={activePlayer==='O'}/>
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSqure={handleSelectSquare} activePlayerSymbol={activePlayer}/>
       </div>
     </main>
   );
